1fa930dc01dea8bbb0de498dc3d0ef6ea432b58e
Jack Clackett <jclackett@gmail.com>
2016-07-26 13:32:59 +0200

    updates to order controll and removing devise

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,11 +10,11 @@ class OrdersController < ApplicationController
 
 	def checkout
 	    @order = Shoppe::Order.find(current_order.id)
-	  	# if request.patch?
+	  	 if request.patch?
 		    if @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
 		      redirect_to checkout_confirmation_path
 		    end
-	  	# end
+	  	 end
 	  	puts current_order.order_items.first.ordered_item.inspect
 	end
 
